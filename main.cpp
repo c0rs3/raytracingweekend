32bd96ee6b339bcdc6d17404f8a4d50a53b82254
Tuna Arıyürek <ariyurektuna@gmail.com>
2024-07-01 16:01:38 +0300

    implement abstraction

--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
-#include "lib/color.h"
-#include "lib/ray.h"
-#include "lib/vec3.h"
+#include "lib/rtweekend.h"
 
-#include <iostream>
+#include "lib/hittable.h"
+#include "lib/hittable_list.h"
+#include "lib/sphere.h"
 
-color ray_color(const ray& r) {
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.0, 0.0, 0.7);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
-
 int main() {
+
+    // Image
+
     auto aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
 
-    // ensure image height is at least 1.
+    // Calculate the image height, and ensure that it's at least 1.
     int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
 
-    // Camera settings
+    // Camera
 
     auto focal_length = 1.0;
     auto viewport_height = 2.0;
@@ -29,12 +36,20 @@ int main() {
     auto viewport_u = vec3(viewport_width, 0, 0);
     auto viewport_v = vec3(0, -viewport_height, 0);
 
+    // World
+
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+
     // Calculate the horizontal and vertical delta vectors from pixel to pixel.
     auto pixel_delta_u = viewport_u / image_width;
     auto pixel_delta_v = viewport_v / image_height;
 
     // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto viewport_upper_left = camera_center
+                             - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
@@ -48,7 +63,7 @@ int main() {
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
