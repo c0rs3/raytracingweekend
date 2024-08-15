f1115e16a01e0f21afefcfeea22818f16da53625
Tuna Arıyürek <ariyurektuna@gmail.com>
2024-08-15 14:36:06 +0300

    better log msgs

--- lib/camera.h
+++ lib/camera.h
@@ -102,7 +102,7 @@ class camera {
         for (auto& thread : threads) {
             thread.join();
         }
-        
+        std::clog << std::endl;
         std::clog << "[" << return_current_time_and_date() <<"] " << "All thread workers finished" << std::endl;
 
         write_bmp("image.bmp", image, image_width, image_height);
