a03596cb61abb0fd45b57bfbb488e6c14aa78826
Tuna Arıyürek <ariyurektuna@gmail.com>
2024-09-16 21:12:32 +0300

    move constructors and operators

--- lib/vec3.h
+++ lib/vec3.h
@@ -1,12 +1,36 @@
 #ifndef VEC3_H
 #define VEC3_H
-
 class vec3 {
   public:
     double e[3];
 
     vec3() : e{0,0,0} {}
     vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+    vec3(const vec3& vec) {
+        e[0] = vec.e[0];
+        e[1] = vec.e[1];
+        e[2] = vec.e[2];
+    }
+
+    vec3(vec3&& vec) noexcept {
+        e[0] = vec.e[0];
+        e[1] = vec.e[1];
+        e[2] = vec.e[2];
+    }
+
+    vec3& operator=(const vec3& vec) {
+        e[0] = vec.e[0];
+        e[1] = vec.e[1];
+        e[2] = vec.e[2];
+        return *this;
+    }
+
+    vec3& operator=(vec3&& vec) noexcept {
+        e[0] = vec.e[0];
+        e[1] = vec.e[1];
+        e[2] = vec.e[2];
+        return *this;
+    }
 
     double x() const { return e[0]; }
     double y() const { return e[1]; }
